7422840b385da0fc38a89c9358eaf65ce5facb67
Cristina Bolohan <cristina.bolohan@irt.de>
2020-11-19 15:00:06 +0100

    Initial Commit

---
+++ AppStore/AppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreApp.swift
+//  AppStore
+//
+//  Created by Cristina Bolohan on 19.11.20.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
